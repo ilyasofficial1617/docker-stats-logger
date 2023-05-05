9b8deb32f02e600ffd3b15c9665605b9e51455cd
ilyasofficial1617 <ilyasofficial1617@gmail.com>
2023-05-05 11:21:22 +0700

    nvidia process monitoring

--- nvidiasmi_logger.sh
+++ nvidiasmi_logger.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export logger_title='nvidiasmi'
-export logger_command='nvidia-smi'
+export logger_command='nvidia-smi pmon -i 0 -c 1 -s um'
 export logger_time='1'
 export logger_bulk='all'
 ./logger.sh
